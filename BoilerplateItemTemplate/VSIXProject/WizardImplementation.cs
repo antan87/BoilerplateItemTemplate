8d47b052319b0ba49c1cad17d848173659994f0e
Anton Patron <patronant@gmail.com>
2019-11-10 18:41:58 +0100

    Initial commit.

---
+++ BoilerplateItemTemplate/VSIXProject/WizardImplementation.cs
@@ -0,0 +1,63 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace VSIXProject
+{
+    public class WizardImplementation : IWizard
+    {
+        private UserInputForm inputForm;
+        private string customMessage;
+
+        // This method is called before opening any item that
+        // has the OpenInEditor attribute.
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        // This method is only called for item templates,
+        // not for project templates.
+        public void ProjectItemFinishedGenerating(ProjectItem
+            projectItem)
+        {
+        }
+
+        // This method is called after the project is created.
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject,
+            Dictionary<string, string> replacementsDictionary,
+            WizardRunKind runKind, object[] customParams)
+        {
+            try
+            {
+                // Display a form to the user. The form collects
+                // input for the custom message.
+                inputForm = new UserInputForm();
+                inputForm.ShowDialog();
+
+                foreach (var item in inputForm.CustomMessages)
+                    replacementsDictionary.Add(item.Key,item.Value);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        // This method is only called for item templates,
+        // not for project templates.
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
